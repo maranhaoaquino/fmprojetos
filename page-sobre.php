19223d54b61f8c003f7c9dc255c9f32125e8a2a7
will <will.santos97@hotmail.com>
2023-03-03 23:31:16 -0300

    atualizando contatos e sobre

--- page-sobre.php
+++ page-sobre.php
@@ -3,7 +3,7 @@
 ?>
 <?php get_header(); ?>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-    <main>
+    <main class="template-middle">
         <h2><?php the_title(); ?><h2>
         <div>
             <?php the_content(); ?>
