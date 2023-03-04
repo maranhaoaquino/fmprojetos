7f04aa1b911d502457c5c6754858cde3cbf3b4a9
will <will.santos97@hotmail.com>
2023-03-04 09:27:31 -0300

    alterações na page contato, page sobre iniciado

--- page-sobre.php
+++ page-sobre.php
@@ -5,7 +5,7 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
     <main class="template-middle">
         <h2><?php the_title(); ?><h2>
-        <div>
+        <div class="">
             <?php the_content(); ?>
         </div>
     </main>
