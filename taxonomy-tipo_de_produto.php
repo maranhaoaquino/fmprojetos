34817de0c39f3364e4a2c4f2a9950f50e7664e17
maranhaoaquino <maranhaoaquino@gmail.com>
2023-03-19 18:03:13 -0300

    ajuste pagina de categorias

--- taxonomy-tipo_de_produto.php
+++ taxonomy-tipo_de_produto.php
@@ -5,39 +5,45 @@
 get_header();
 $term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) );
 ?>
-<div class="wrapper">
-  <div class="primary-content">
-    <h1 class="archive-title"><?php echo apply_filters( 'the_title', $term->name ); ?></h1>
 
-    <?php if ( have_posts() ): while ( have_posts() ): the_post(); ?>
+  <main class="template-middle">
+    <h2 class="archive-title"><?php echo apply_filters( 'the_title', $term->name ); ?></h2>
+    <div class="lista-produtos">
+      <?php if ( have_posts() ): while ( have_posts() ): the_post(); ?>
+      <a href="<?php the_permalink(); ?>" rel="bookmark">
+        <div class="card" id="post-<?php the_ID(); ?>" <?php post_class('post clearfix'); ?>>
+            <div class="card-header">
+                <img src="<?php the_field_cmb2('imagem_principal_produto'); ?>" alt="<?php the_title(); ?>" height="100%">
+            </div>
+            <div class="card-body">
+                <h5><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h5>
+                <a href="<?php the_permalink(); ?>" rel="bookmark" class="btn-preco">
+                    Preço Sob Consulta
+                </a>
+                <p>
+                  <?php the_content( __('Full story…') ); ?>
+                </p>
+            </div>
+        </div>
+      </a>
+      <?php endwhile; ?>
 
-    <div id="post-<?php the_ID(); ?>" <?php post_class('post clearfix'); ?>>
-      <h2 class="post-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+      <div class="navigation clearfix">
+        <div class="alignleft"><?php next_posts_link('« Previous Entries') ?></div>
+        <div class="alignright"><?php previous_posts_link('Next Entries »') ?></div>
+      </div>
+
+      <?php else: ?>
+
+      <h2 class="post-title">No News in <?php echo apply_filters( 'the_title', $term->name ); ?></h2>
       <div class="content clearfix">
-        <img src="<?php the_field_cmb2('imagem_principal_produto'); ?>">
         <div class="entry">
-          <?php the_content( __('Full story…') ); ?>
+          <p>It seems there isn't anything happening in <strong><?php echo apply_filters( 'the_title', $term->name ); ?></strong> right now. Check back later, something is bound to happen soon.</p>
         </div>
       </div>
-    </div><!--// end #post-XX -->
 
-    <?php endwhile; ?>
-
-    <div class="navigation clearfix">
-      <div class="alignleft"><?php next_posts_link('« Previous Entries') ?></div>
-      <div class="alignright"><?php previous_posts_link('Next Entries »') ?></div>
+      <?php endif; ?>
     </div>
-
-    <?php else: ?>
-
-    <h2 class="post-title">No News in <?php echo apply_filters( 'the_title', $term->name ); ?></h2>
-    <div class="content clearfix">
-      <div class="entry">
-        <p>It seems there isn't anything happening in <strong><?php echo apply_filters( 'the_title', $term->name ); ?></strong> right now. Check back later, something is bound to happen soon.</p>
-      </div>
-    </div>
-
-    <?php endif; ?>
-  </div><!--// end .primary-content -->
+  </main>
 
 <?php get_footer(); ?>
