7dcdc50ac4cd6cc65c44b650dbd546ae55774183
maranhaoaquino <maranhaoaquino@gmail.com>
2023-03-25 15:58:12 -0300

    logo

--- functions.php
+++ functions.php
@@ -136,4 +136,17 @@ function create_custom_tax_tipo(){
     register_taxonomy( $custom_tax_nome, $custom_post_type_nome, $args );
 }
 
+function themename_custom_logo_setup() {
+	$defaults = array(
+		'height'               => 100,
+		'width'                => 400,
+		'flex-height'          => true,
+		'flex-width'           => true,
+		'header-text'          => array( 'site-title', 'site-description' ),
+		'unlink-homepage-logo' => true, 
+	);
+	add_theme_support( 'custom-logo', $defaults );
+}
+add_action( 'after_setup_theme', 'themename_custom_logo_setup' );
+
 ?>
--- header.php
+++ header.php
@@ -22,7 +22,15 @@
         <div class="header-class">
             <!-- ESPACO PARA LOGO -->
             <div class="logo">
-                <h1><?php bloginfo('name'); ?></h1>
+                <?php
+                    $custom_logo_id = get_theme_mod( 'custom_logo' );
+                    $logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+                    if ( has_custom_logo() ) {
+                        echo '<img src="' . esc_url( $logo[0] ) . '" alt="' . get_bloginfo( 'name' ) . '">';
+                    } else {
+                        echo '<h1>' . get_bloginfo('name') . '</h1>';
+                    }
+                ?>
             </div>
             <!-- NAVEGAÇÃO -->
             <div class="navmap">
