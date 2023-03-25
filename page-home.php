53d3452763bc5ee79073a8dba0ddd0fc79e50738
will <will.santos97@hotmail.com>
2023-03-25 19:00:03 -0300

    responsividade home ok

--- page-home.php
+++ page-home.php
@@ -29,7 +29,7 @@
                 });
             </script>
             <!-- PRODUTOS -->
-            <div class="nossos-produtos">
+            <div class="componentes-home">
                 <h2>Nossos Produtos</h2>
                 <!-- ESPAÇO PARA OS CARD DOS PRODUTOS -->
                 <div class="lista-produtos">
@@ -64,12 +64,12 @@
                     <?php wp_reset_query(); wp_reset_postdata(); ?>           
                 </div>
 
-                <button class="btn-mais-produtos">
+                <button class="btn-black-home">
                     Veja nossos Produtos
                 </button>
             </div>
             <!-- Qualidade -->
-            <div class="qualidade">
+            <div class="componentes-home">
                 <h2>Qualidade Garantida</h2>
                 <!-- Slide Principal -->
                 <?php $vantagens = get_field_cmb2('vantagens'); 
@@ -92,50 +92,60 @@
                 <a href="#orçamento" class="btn-primary">Fale com a gente</a>
             </div>
             <!-- PROJETOS -->
-            <div class="projetos">
-                <h2>Projetos Realizados</h2>
-                <div class="fotos-projetos">
-                    <?php
-                        $args = array (
-                            'post_type' => 'projetos',
-                            'order'   => 'ASC'
-                        );
-                        $the_query = new WP_Query ( $args );
-                    ?>
-                    <?php if ( $the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-                        <a href="<?php the_permalink(); ?>" rel="bookmark">
-                            <div id="post-<?php the_ID(); ?>" <?php post_class('post clearfix'); ?>>
-                                <?php $slide_projeto = get_field_cmb2('slide_projeto'); 
-                                    if(isset($slide_projeto)) {
-                                ?>
-                                <div class="foto-projeto-home">
-                                    <div class="item">
-                                        <img src="<?php echo $slide_projeto[0]['foto_projeto'] ?>">
+            <div class="componentes-home">
+                <h2>Projetos Realizados</h2>                
+                    <div class="fotos-projetos">
+                        <?php
+                            $args = array (
+                                'post_type' => 'projetos',
+                                'order'   => 'ASC'
+                            );
+                            $the_query = new WP_Query ( $args );
+                        ?>
+                        <?php if ( $the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+                            <a href="<?php the_permalink(); ?>" rel="bookmark">
+                                <div id="post-<?php the_ID(); ?>" <?php post_class('post clearfix'); ?>>
+                                    <?php $slide_projeto = get_field_cmb2('slide_projeto'); 
+                                        if(isset($slide_projeto)) {
+                                    ?>
+                                    <div class="foto-projeto-home">
+                                        <div class="item-css">
+                                            <img src="<?php echo $slide_projeto[0]['foto_projeto'] ?>" class="img-projetos">
+                                            <img src="<?php echo $slide_projeto[1]['foto_projeto'] ?>" class="img-projetos">
+                                            <img src="<?php echo $slide_projeto[2]['foto_projeto'] ?>" class="img-projetos">
+                                            <img src="<?php echo $slide_projeto[3]['foto_projeto'] ?>" class="img-projetos">
+                                            <img src="<?php echo $slide_projeto[4]['foto_projeto'] ?>" class="img-projetos">
+                                            <img src="<?php echo $slide_projeto[5]['foto_projeto'] ?>" class="img-projetos">
+                                        </div>
                                     </div>
+                                    <?php } ?>
                                 </div>
-                                <?php } ?>
-                            </div>
-                        </a>
-                    <?php endwhile; else: endif; ?>
-                    <?php wp_reset_query(); wp_reset_postdata(); ?>
-                </div>
+                            </a>
+                        <?php endwhile; else: endif; ?>
+                        <?php wp_reset_query(); wp_reset_postdata(); ?>
+                    </div>
+                    <button class="btn-black-home">
+                    Veja nossos Projetos
+                </button> 
             </div>
             <!-- PARCEIROS -->
-            <div class="parceiros">
+            <div class="componentes-home">
                 <h2>Nossos Parceiros</h2>
                 <?php $parceiros = get_page_by_title('Nossos Parceiros')->ID; ?>
                 <?php $marca_parceiro = get_field_cmb2('marca_parceiro', $parceiros); 
                 if(isset($marca_parceiro)) {
                 ?>
-                <div id="slide-parceiros" class="owl-carousel owl-theme">
-                    <?php foreach($marca_parceiro as $slide) { ?>
-                        <div class="item">
-                            <a href="<?php echo $slide['link_parceiro'] ?>">
-                                <img src="<?php echo $slide['logo_parceiro'] ?>">
-                                <h3><?php echo $slide['nome_parceiro'] ?></h3>
-                            </a>
-                        </div>
-                    <?php } ?>
+                <div class="item-css">
+                    <div id="slide-parceiros " class="owl-carousel owl-theme">
+                        <?php foreach($marca_parceiro as $slide) { ?>
+                            <div class="item">
+                                <a href="<?php echo $slide['link_parceiro'] ?>">
+                                    <img src="<?php echo $slide['logo_parceiro'] ?>">
+                                    <h3><?php echo $slide['nome_parceiro'] ?></h3>
+                                </a>
+                            </div>
+                        <?php } ?>
+                    </div>
                 </div>
                 <?php } ?>
                 <script>
@@ -161,21 +171,23 @@
                 </script>
             </div>
             <!-- Clientes -->
-            <div class="clientes">
+            <div class="componentes-home">
                 <h2>Nossos Clientes</h2>
                 <?php $clientes = get_page_by_title('Nossos Clientes')->ID; ?>
                 <?php $marca_cliente = get_field_cmb2('marca_cliente', $clientes); 
                 if(isset($marca_cliente)) {
                 ?>
-                <div id="slide-clientes" class="owl-carousel owl-theme">
-                    <?php foreach($marca_cliente as $slide) { ?>
-                        <div class="item">
-                            <a href="<?php echo $slide['link_cliente'] ?>">
-                                <img src="<?php echo $slide['logo_cliente'] ?>">
-                                <h3><?php echo $slide['nome_cliente'] ?></h3>
-                            </a>
-                        </div>
-                    <?php } ?>
+                <div class="item-css">
+                    <div id="slide-clientes" class="owl-carousel owl-theme">
+                        <?php foreach($marca_cliente as $slide) { ?>
+                            <div class="item">
+                                <a href="<?php echo $slide['link_cliente'] ?>">
+                                    <img src="<?php echo $slide['logo_cliente'] ?>">
+                                    <h3><?php echo $slide['nome_cliente'] ?></h3>
+                                </a>
+                            </div>
+                        <?php } ?>
+                    </div>
                 </div>
                 <?php } ?>
                 <script>
