22523cb9aa2e1893b33a48f3019942a0973f6681
will <will.santos97@hotmail.com>
2023-04-07 10:25:47 -0300

    atualizacao icone whatsapp

--- footer.php
+++ footer.php
@@ -56,7 +56,7 @@
         </section>
     </footer>
     <div class="whatsapp">
-        <a href="https://wa.me/5571993295049" target="_blank"> <img src="<?php echo get_template_directory_uri(); ?>/img/whatsapp-icone-1.png" alt="botão whatsapp"></a>
+        <a href="https://wa.me/5571993295049" target="_blank"> <img src="<?php echo get_template_directory_uri(); ?>/img/zapquadrado.png" alt="botão whatsapp"></a>
     </div>
     <?php wp_footer(); ?>
 </body>
