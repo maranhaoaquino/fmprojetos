253fdad72bca6a7c54b39e556d77b6affed8a2aa
will <will.santos97@hotmail.com>
2023-04-24 20:47:05 -0300

    bug no cache

--- page-contato.php
+++ page-contato.php
@@ -10,43 +10,60 @@
                 <iframe src="<?php the_field_cmb2('link_mapa');?>" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
             <div class="contatos">     
-                <div class="input-effect">
-                    <i class="fa fa-phone" aria-hidden="true"></i>
-                    <p>
-                        <?php the_field_cmb2('telefone'); ?>
-                    </p>
-                </div>
-                <div class="input-effect">
-                    <i class="fa fa-envelope" aria-hidden="true"></i>
-                    <p>
-                        <?php the_field_cmb2('email'); ?>
-                    </p>
-                </div>
-                <div class="input-effect">
-                    <i class="fa fa-street-view" aria-hidden="true"></i>
-                    <p>
-                        <?php the_field_cmb2('endereco'); ?>
-                    </p>
-                </div>
-                <div class="input-effect">
-                    <i class="fa fa-whatsapp" aria-hidden="true"></i>
-                    <p>
-                        <?php the_field_cmb2('whatsapp'); ?>
-                    </p>
-                </div>
-                <div class="input-effect">
-                    <i class="fa fa-instagram" aria-hidden="true"></i>
-                    <p>
-                        <?php the_field_cmb2('instagram'); ?>
-                    </p>
-                </div>
-                <div class="input-effect">
-                    <i class="fa fa-facebook-official" aria-hidden="true"></i>
-                    <p>
-                        <?php the_field_cmb2('facebook'); ?>
-                    </p>
-                </div>
-                                                                                                                                       
+            
+                <?php if (!empty(the_field_cmb2('telefone'))) { ?>
+                    <div class="input-effect">
+                        <i class="fa fa-phone" aria-hidden="true"></i>
+                        <span>
+                            <?php the_field_cmb2('telefone'); ?>
+                        </span>
+                    </div>
+                <?php } ?>
+
+                <?php if (!empty(the_field_cmb2('email'))) { ?>
+                    <div class="input-effect">
+                        <i class="fa fa-envelope" aria-hidden="true"></i>
+                        <span>
+                            <?php the_field_cmb2('email'); ?>
+                        </span>
+                    </div>
+                <?php } ?>
+
+                <?php if (!empty(the_field_cmb2('endereco'))) { ?>
+                    <div class="input-effect">
+                        <i class="fa fa-street-view" aria-hidden="true"></i>
+                        <span>
+                            <?php the_field_cmb2('endereco'); ?>
+                        </span>
+                    </div>
+                <?php } ?>
+
+                <?php if (!empty(the_field_cmb2('whatsapp'))) { ?>
+                    <div class="input-effect">
+                        <i class="fa fa-whatsapp" aria-hidden="true"></i>
+                        <span>
+                            <?php the_field_cmb2('whatsapp'); ?>
+                        </span>
+                    </div>
+                <?php } ?>
+
+                <?php if (!empty(the_field_cmb2('instagram'))) { ?>
+                    <div class="input-effect">
+                        <i class="fa fa-instagram" aria-hidden="true"></i>
+                        <span>
+                            <?php the_field_cmb2('instagram'); ?>
+                        </span>
+                    </div>
+                <?php } ?>
+
+                <?php if (!empty(the_field_cmb2('instagram'))) { ?>
+                    <div class="input-effect">
+                        <i class="fa fa-facebook-official" aria-hidden="true"></i>
+                        <span>
+                            <?php the_field_cmb2('facebook'); ?>
+                        </span>
+                    </div>
+                <?php } ?>                   
             </div>
         </div>
     </main>
--- page-sobre.php
+++ page-sobre.php
@@ -3,7 +3,7 @@
 ?>
 <?php get_header(); ?>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-    <main class="template-middle">
+    <main class="container p-5">
         <h2><?php the_title(); ?><h2>
         <div class="page-sobre-area">            
                 <?php the_content(); ?>
