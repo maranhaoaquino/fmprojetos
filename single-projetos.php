22e5ab276c58cb3ed7181cd9a30bccb8b4d9ffde
maranhaoaquino <maranhaoaquino@gmail.com>
2023-04-29 17:24:41 -0300

    ajuste projeto

--- single-projetos.php
+++ single-projetos.php
@@ -3,11 +3,15 @@
 ?>
 <?php get_header(); ?>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-    <main class="template-middle">
-        <h2><?php the_title(); ?><h2>
-        <?php $slide_projeto = get_field_cmb2('slide_projeto'); 
-            if(!empty($slide_projeto)) {
-        ?>
+    <main>
+        <section class="container mt-5">
+            <div class="row text-center">
+                <h2><?php the_title(); ?><h2>
+            </div>
+        </section>
+        <section class="container mt-3 mb-3">
+            <?php $slide_projeto = get_field_cmb2('slide_projeto'); ?>
+            <?php if(!empty($slide_projeto)) { ?>
                 <div id="slide-projeto" class="owl-carousel owl-theme">
                     <?php foreach($slide_projeto as $slide) { ?>
                         <div class="item">
@@ -15,31 +19,35 @@
                         </div>
                     <?php } ?>
                 </div>
-                <?php } ?>
-                <script>
-                    $('#slide-projeto').owlCarousel({
-                        loop:false,
-                        nav:true,
-                        dots: true,
-                        responsive:{
-                            0:{
-                                items: 3,
-                                margin: 10
-                            },
-                            711:{
-                                items: 4,
-                                margin: 20
-                            },
-                            1023:{
-                                items: 6,
-                                margin: 30
-                            }
-                        }
-                    });
-                </script>
-        <div class="page-sobre-area">            
+            <?php } ?>
+        </section>
+        <section class="container">
+            <div class="row">
                 <?php the_content(); ?>
-        </div>
+            </div>
+        </section>            
     </main>
 <?php endwhile; endif; ?>
+<script>
+    $('#slide-projeto').owlCarousel({
+        loop:false,
+        nav:true,
+        dots: true,
+        autoWidth:true,
+        responsive:{
+            0:{
+                items: 3,
+                margin: 10
+            },
+            711:{
+                items: 4,
+                margin: 20
+            },
+            1023:{
+                items: 6,
+                margin: 30
+            }
+        }
+    });
+</script>
 <?php get_footer(); ?>
--- taxonomy-tipo_de_projeto.php
+++ taxonomy-tipo_de_projeto.php
@@ -6,46 +6,56 @@ get_header();
 $term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) );
 ?>
 
-  <main class="template-middle">
-    <h2 class="archive-title"><?php echo apply_filters( 'the_title', $term->name ); ?></h2>
-    <div class="carrosseis-projetos">
-      <?php if ( have_posts() ): while ( have_posts() ): the_post(); ?>
-      <a href="<?php the_permalink(); ?>" rel="bookmark">
-        <div id="post-<?php the_ID(); ?>" <?php post_class('post clearfix'); ?>>
-          <div>
-            <h3><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h3>
-          </div>
-          <?php $slide_projeto = get_field_cmb2('slide_projeto'); 
-              if(!empty($slide_projeto)) {
-          ?>
-            <div class="slide-projeto owl-carousel owl-theme">
-              <?php foreach($slide_projeto as $slide) { ?>
-                <div class="item">
-                  <img src="<?php echo $slide['foto_projeto'] ?>" width="100px" height="100px">
+  <main>
+    <section class="container mt-5">
+      <div class="row text-center">
+        <h2 class="archive-title"><?php echo apply_filters( 'the_title', $term->name ); ?></h2>
+      </div>
+    </section>
+    
+    <?php if ( have_posts() ): while ( have_posts() ): the_post(); ?>
+      <section class="container mt-3">
+        <div class="row">
+          <a href="<?php the_permalink(); ?>" rel="bookmark">
+            <div id="post-<?php the_ID(); ?>" <?php post_class('post clearfix'); ?>>
+              <div class="text-align-start">
+                <h3><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h3>
+              </div>
+              <?php $slide_projeto = get_field_cmb2('slide_projeto'); 
+                  if(!empty($slide_projeto)) {
+              ?>
+                <div class="slide-projeto owl-carousel owl-theme">
+                  <?php foreach($slide_projeto as $slide) { ?>
+                    <div class="item">
+                      <img src="<?php echo $slide['foto_projeto'] ?>">
+                    </div>
+                  <?php } ?>
                 </div>
               <?php } ?>
             </div>
-          <?php } ?>
+          </a>
         </div>
-      </a>
-      <?php endwhile; ?>
-
+      </section>
+    <?php endwhile; ?>
+    
+    <section class="container">
       <div class="navigation clearfix">
         <div class="alignleft"><?php next_posts_link('« Previous Entries') ?></div>
         <div class="alignright"><?php previous_posts_link('Next Entries »') ?></div>
       </div>
+    </section>
 
-      <?php else: ?>
+    <?php else: ?>
 
-      <h2 class="post-title">No News in <?php echo apply_filters( 'the_title', $term->name ); ?></h2>
-      <div class="content clearfix">
-        <div class="entry">
-          <p>It seems there isn't anything happening in <strong><?php echo apply_filters( 'the_title', $term->name ); ?></strong> right now. Check back later, something is bound to happen soon.</p>
+      <section class="container mt-5">
+        <h2 class="post-title">Nenhum projeto encontrado</h2>
+        <div class="content clearfix">
+          <div class="entry">
+            <p>Nenhum projeto encontrado</p>
+          </div>
         </div>
-      </div>
-
-      <?php endif; ?>
-    </div>
+      </section>
+    <?php endif; ?>
   </main>
   <script>
     $('.slide-projeto').owlCarousel({
