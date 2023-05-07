e4d180fd2f53281b9738f86d0fdcc4f45a7a8580
maranhaoaquino <maranhaoaquino@gmail.com>
2023-05-07 12:53:37 -0300

    slide na taxonomia

--- taxonomy-tipo_de_produto.php
+++ taxonomy-tipo_de_produto.php
@@ -7,6 +7,34 @@ $term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' 
 ?>
 
   <main>
+    <div id="slide-principal" class="owl-carousel owl-theme">
+      <?php if ( have_posts() ): while ( have_posts() ): the_post(); ?>
+        <?php foreach($fotos_equipamentos as $slide) { ?>
+          <?php if(!empty($slide['foto_equipamento'])) { ?>
+            <div class="item">
+              <a href="<?php the_permalink(); ?>">
+                <img src="<?php echo $slide['foto_equipamento'] ?>">
+                  <div id="grid-slider">
+                    <h2><?php the_title();?></h2>
+                  </div>
+              </a>
+            </div>
+          <?php } ?>
+        <?php } ?>
+      <?php endwhile; ?>
+    </div>
+    <script>
+      $('#slide-principal').owlCarousel({
+        loop:false,
+        nav:true,
+        items: 1,
+        dots: true,
+        navText: [
+          "<i class='fa fa-chevron-left'></i>",
+          "<i class='fa fa-chevron-right'></i>"
+        ],
+      });
+    </script>
     <section class="container mt-5">
       <div class="row text-center">
         <h2 class="archive-title"><?php echo apply_filters( 'the_title', $term->name ); ?></h2>
