18f92eb40d9a9aa7741a89c075abbd6c1fb6ecc5
maranhaoaquino <maranhaoaquino@gmail.com>
2023-05-07 14:23:51 -0300

    ajuste card de produtos

--- inc/card-produtos.php
+++ inc/card-produtos.php
@@ -4,21 +4,10 @@
             <div class="card-header">
                 <img src="<?php the_field_cmb2('imagem_principal_produto'); ?>" alt="<?php the_title(); ?>">
             </div>
-            <div class="card-body"  id="body-dynamic">
+            <div class="description-card">
                 <a href="<?php the_permalink(); ?>" rel="bookmark" class="card-produtos-titulo"><h5><?php the_title();?></h5></a>
-
                 <?php the_excerpt(); ?>
             </div>
         </div>
     </a>
 </div>
-<script type="text/javascript">
-    let bodyDynamic = document.querySelector(".card-body");
-    
-    function setCardBody() {
-        console.log(bodyDynamic.style);
-        bodyDynamic.style.display = "block"
-        
-    }
-
-</script>
